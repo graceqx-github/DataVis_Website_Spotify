76ec6cac27992601aa68af30c8a238d747853a4e
Gracie <69355143+GraceQX@users.noreply.github.com>
2023-11-21 22:30:47 +0800

    adjust index2 layout

--- src/js/radar.js
+++ src/js/radar.js
@@ -1,39 +1,44 @@
 // Constants and initializations for Radar Chart
 const radarPath = 'dataset.csv'; // Adjust the path to your dataset
 const radarWidth = 400;
-const radarHeight = 500;
+const radarHeight = 400;
 const radarRadius = Math.min(radarWidth, radarHeight) / 2 - 20;
 const radarSvg = d3.select("#radarChart");
 const radarDimensions = ['danceability', 'liveness_norm', 'tempo_norm', 'energy', 'valence', 'loudness_norm', 'speechiness_norm', 'acousticness_norm'];
+const radarName = ['danceability', 'liveness', 'tempo', 'energy', 'valence', 'loudness', 'speechiness', 'acousticness'];
 const radarAngleSlice = Math.PI * 2 / radarDimensions.length;
 const radarGenreColors = {
-    "punk": "rgb(187, 215, 239,0.7)",
+    "metal": "rgb(187, 215, 239,0.7)",
     "pop": "rgb(136, 250, 132,0.7)",
-    "acoustic": "rgb(236, 108, 108,0.7)"
+    "acoustic": "rgb(236, 108, 108,0.7)",
+    'world-music':"rgb(229, 206, 198)"
 };
+const radarStrokeColors = {
+    'world-music':"rgb(239, 216, 208)"
+}
 
 // Current genre selection
-let currentGenre = "punk"; // Default genre
+let currentGenre = "world-music"; // Default genre
 
 // Initial Setup for Radar Chart
 // Drawing radar circles
 for (let i = 0; i < radarDimensions.length; i++) {
     const factor = radarRadius / radarDimensions.length * (i + 1);
     radarSvg.append("circle")
-        .attr("cx", radarWidth / 2)
+        .attr("cx", radarWidth / 2-20)
         .attr("cy", radarHeight / 2)
         .attr("r", factor)
         .attr("class", "radar-circle");
 }
-
+console.error("initialize");
 // Drawing radar lines and dimension names
 radarDimensions.forEach((dim, i) => {
-    const x = radarWidth / 2 + radarRadius * Math.cos(radarAngleSlice * i - Math.PI / 2);
+    const x = radarWidth / 2-20 + radarRadius * Math.cos(radarAngleSlice * i - Math.PI / 2);
     const y = radarHeight / 2 + radarRadius * Math.sin(radarAngleSlice * i - Math.PI / 2);
 
     // Radar lines
     radarSvg.append("line")
-        .attr("x1", radarWidth / 2)
+        .attr("x1", radarWidth / 2-20)
         .attr("y1", radarHeight / 2)
         .attr("x2", x)
         .attr("y2", y)
@@ -43,11 +48,11 @@ radarDimensions.forEach((dim, i) => {
     radarSvg.append("text")
         .attr("x", x)
         .attr("y", y - 10)
-        .attr("text-anchor", "middle")
+        .attr("text-anchor", "right")
         .attr("fill", "rgb(187, 215, 239)")
-        .text(dim);
+        .text(radarName[i]);
 });
-
+console.error("initialize");
 // Helper function to update and draw the radar for a specific year and genre
 const drawRadarForGenre = (year, genre) => {
     radarSvg.selectAll(".polygon-genre").remove(); // Clear existing chart
@@ -58,27 +63,19 @@ const drawRadarForGenre = (year, genre) => {
             const releaseYear = d.release_date.split('/')[2];
             return releaseYear === year && d.new_genre === genre;
         });
-
-        // Compute dimension mins and maxes
-        const dimensionMins = {};
-        const dimensionMaxs = {};
-        radarDimensions.forEach(dim => {
-            dimensionMins[dim] = d3.min(filteredData, d => +d[dim]);
-            dimensionMaxs[dim] = d3.max(filteredData, d => +d[dim]);
-        });
-
+        console.log("filteredData:",filteredData);
         // Compute average values for the selected genre
         const averages = {};
         radarDimensions.forEach(dim => {
             averages[dim] = d3.mean(filteredData, d => +d[dim]);
         });
-        // console.log("Averages:", averages);
+        console.log("Averages:", averages);
 
         // Compute normalized average coordinates for selected genre
         const lineGenerator = d3.line().curve(d3.curveLinearClosed);
         const averagePoints = radarDimensions.map((dim, i) => {
-            const value = (averages[dim] - dimensionMins[dim]) / (dimensionMaxs[dim] - dimensionMins[dim]);
-            const x = radarWidth / 2 + radarRadius * value * Math.cos(radarAngleSlice * i - Math.PI / 2);
+            const value = averages[dim];
+            const x = radarWidth / 2-20 + radarRadius * value * Math.cos(radarAngleSlice * i - Math.PI / 2);
             const y = radarHeight / 2 + radarRadius * value * Math.sin(radarAngleSlice * i - Math.PI / 2);
             return [x, y];
         });
@@ -89,57 +86,11 @@ const drawRadarForGenre = (year, genre) => {
             .attr("d", lineGenerator(averagePoints))
             .attr("class", "polygon-genre")
             .attr("fill", radarGenreColors[genre])
+            .attr("stroke", d3.rgb(radarGenreColors[genre]).darker())
+            .attr("stroke-width", 5)
             .style("opacity", 0.7)
             .transition()
             .duration(500);
-    });
-};
-
-// Function to draw all genres
-const drawAllGenres = (year) => {
-    radarSvg.selectAll(".polygon-genre").remove(); // Clear existing chart only once at the beginning
-
-    const genres = ["punk", "pop", "acoustic"]; // List of genres
-    genres.forEach(genre => {
-        d3.csv(radarPath).then(data => {
-            // Filter data based on the selected year and genre
-            const filteredData = data.filter(d => {
-                const releaseYear = d.release_date.split('/')[2];
-                return releaseYear === year && d.new_genre === genre;
-            });
-    
-            // Compute dimension mins and maxes
-            const dimensionMins = {};
-            const dimensionMaxs = {};
-            radarDimensions.forEach(dim => {
-                dimensionMins[dim] = d3.min(filteredData, d => +d[dim]);
-                dimensionMaxs[dim] = d3.max(filteredData, d => +d[dim]);
-            });
-    
-            // Compute average values for the selected genre
-            const averages = {};
-            radarDimensions.forEach(dim => {
-                averages[dim] = d3.mean(filteredData, d => +d[dim]);
-            });
-    
-            // Compute normalized average coordinates for selected genre
-            const lineGenerator = d3.line().curve(d3.curveLinearClosed);
-            const averagePoints = radarDimensions.map((dim, i) => {
-                const value = (averages[dim] - dimensionMins[dim]) / (dimensionMaxs[dim] - dimensionMins[dim]);
-                const x = radarWidth / 2 + radarRadius * value * Math.cos(radarAngleSlice * i - Math.PI / 2);
-                const y = radarHeight / 2 + radarRadius * value * Math.sin(radarAngleSlice * i - Math.PI / 2);
-                return [x, y];
-            });
-    
-            // Draw the polygon for the genre
-            radarSvg.append("path")
-                .attr("d", lineGenerator(averagePoints))
-                .attr("class", "polygon-genre")
-                .attr("fill", radarGenreColors[genre])
-                .style("opacity", 0.7)
-                .transition()
-                .duration(500);
-        });
     });
 };
 
@@ -154,18 +105,6 @@ document.getElementById("yearSlider").addEventListener("input", function(event) 
     }
 });
 
-// Interaction: Attach click event listeners to the genre controls
-document.querySelectorAll('.genre-controls label').forEach(label => {
-    label.addEventListener('click', function() {
-        currentGenre = this.getAttribute('data-genre');
-        const selectedYear = document.getElementById("yearSlider").value;
-        if (currentGenre === "all-genres") {
-            drawAllGenres(selectedYear);
-        } else {
-            drawRadarForGenre(selectedYear, currentGenre);
-        }
-    });
-});
 
 // Initial radar chart for a default year and genre
 drawRadarForGenre("2000", currentGenre);
--- src/js/ridge.js
+++ src/js/ridge.js
@@ -1,10 +1,10 @@
 // Set the dimensions and margins of the graph
-var margin = {top: 130, right: 50, bottom: 70, left: 120}, // Increased top margin
+var margin = {top: 130, right: 50, bottom: 30, left: 120}, // Increased top margin
     width_ridge = 480 - margin.left - margin.right,
     height_ridge = 400 - margin.top - margin.bottom;
 
 // Append the svg object to the body of the page
-var svg = d3.select("#ridge_chart")
+var svg = d3.select("#ridgeChart")
   .append("svg")
     .attr("width", width_ridge + margin.left + margin.right)
     .attr("height", height_ridge + margin.top + margin.bottom)
@@ -12,12 +12,13 @@ var svg = d3.select("#ridge_chart")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 var categories = ['danceability', 'liveness_norm', 'tempo_norm', 'energy', 'valence', 'loudness_norm', 'speechiness_norm', 'acousticness_norm'];
+const radarName = ['danceability', 'liveness', 'tempo', 'energy', 'valence', 'loudness', 'speechiness', 'acousticness'];
 var n = categories.length;
 var selectedGenre = "world-music";
 
 // Function to update the ridge plot
 function updateRidgePlot(selectedYear, selectedGenre) {
-    d3.csv("../dataSource/dataset.csv").then(function(data) {
+    d3.csv("dataset.csv").then(function(data) {
         // Filter data by year and genre
         var filteredData = data.filter(function(d) {
             var year = d.release_date.split('/')[2];
